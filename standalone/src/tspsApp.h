d18327c712749c87b8d450d721cf692591050927
Emmanuel Geoffray <manuel@soixantecircuits.fr>
2012-02-24 06:10:22 +0100

    about to leave to boulogne

--- standalone/src/tspsApp.h
+++ standalone/src/tspsApp.h
@@ -10,7 +10,7 @@
     // TSPS core
     #include "ofxTSPS.h"
 
-    //#define _USE_LIVE_VIDEO         // comment out to load a movie file
+    #define _USE_LIVE_VIDEO         // comment out to load a movie file
     //#define USE_CUSTOM_GUI		// uncomment to add a "custom" panel to the gui
 	
     // kinect support
